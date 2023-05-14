b3a7445598d44e85e505c4ee10830e4c03175f80
PanDa <102406915+xiongdonghai@users.noreply.github.com>
2023-05-14 21:31:20 +0800

    2023/5/14
    跟新轮博图

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const arr = [{
-    title: '一步一个脚印'
-}, {
-    title: '问多了全是问题 做多了全是答案'
-}, {
-    title: '向上的路，并不拥挤。拥挤是因为，大部分人选择了安逸'
-}, {
-    title: '加油,后盾人'
-}];
-let getSectioP = document.querySelector('section p');
-let i = 0;
-setInterval(function() {
-    getSectioP.innerHTML = arr[i].title;
-    i++;
-    if (i >= arr.length) {
-        i = 0;
-    }
-}, 1000);
+function sectionP() {
+    const arr = [{
+        title: '一步一个脚印'
+    }, {
+        title: '问多了全是问题 做多了全是答案'
+    }, {
+        title: `向上的路，并不拥挤。<br>拥挤是因为，<br>大部分人选择了安逸`
+    }, {
+        title: '加油后盾人'
+    }];
+    let getSectioP = document.querySelector('section p');
+    let i = 0;
+    setInterval(function() {
+        i = (i + 1) % arr.length;
+        getSectioP.innerHTML = arr[i].title;
+    }, 3000);
+}
+sectionP();
+
+function sectionImg() {
+    let getSectioImg = document.querySelector('section img');
+    let images = ['https://picsum.photos/224/224', 'https://picsum.photos/225/225', 'https://picsum.photos/226/226'];
+    let i = 0;
+    setInterval(function() {
+        getSectioImg.src = images[i];
+        i = (i + 1) % images.length;
+    }, 6000);
+}
+sectionImg();
